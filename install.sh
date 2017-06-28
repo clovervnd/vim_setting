870a7b1a11f39c59d8037428f95e7df36809ba1d
Joonki <jkhong12000@gmail.com>
2017-06-28 14:00:13 +0900

    save

--- install.sh
+++ install.sh
@@ -4,3 +4,9 @@ rm ~/.vimrc_old
 mv ~/.vimrc ~/.vimrc_old
 cp .vimrc ~/ 
 vim -c 'PluginInstall' -c 'qa!'
+git clone https://github.com/vim-scripts/SrcExpl.git 
+git clone https://github.com/vim-scripts/Trinity.git
+
+cp -r SrcExpl/plugin/srcexpl.vim ~/.vim/plugin/ 
+cp -r Trinity/plugin/trinity.vim ~/.vim/plugin/ 
+
